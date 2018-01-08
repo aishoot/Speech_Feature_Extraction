12d5632747fd1321138f7787dd8b506e2ed99b56
Legendary Ma <18482571+pchao6@users.noreply.github.com>
2018-01-08 22:00:56 +0800

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from base import delta
 from base import logfbank
 import scipy.io.wavfile as wav
 
-(rate,sig) = wav.read("english.wav")
+(rate,sig) = wav.read("sounds/english.wav")
 mfcc_feat = mfcc(sig, rate)
 d_mfcc_feat = delta(mfcc_feat, 2)
 fbank_feat = logfbank(sig, rate)
